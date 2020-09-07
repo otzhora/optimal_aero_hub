7e9465aa78cbabea5a14a2eb04f0c35532ed1ab7
yuriy_rogachev <Yuriy.Rogachev@jetbrains.com>
2020-09-07 15:16:10 +0300

    switched to pytest

--- tests/test_geodesic.py
+++ tests/test_geodesic.py
@@ -1,20 +1,15 @@
-import unittest
+import pytest
 
 from hub.geodesic import Point, distance
 
 
-class TestGeodesic(unittest.TestCase):
-    def test_distance(self):
-        moscow = Point(55.751244, 37.618423)
-        spb = Point(59.9342802, 30.3350986)
+def test_distance():
+    moscow = Point(55.751244, 37.618423)
+    spb = Point(59.9342802, 30.3350986)
 
-        self.assertAlmostEqual(633.4538325946983, distance(moscow, spb))
+    assert 633.4538325946983 == pytest.approx(distance(moscow, spb))
 
-        newport = Point(41.49008, -71.312796)
-        cleveland = Point(41.499498, -81.695391)
+    newport = Point(41.49008, -71.312796)
+    cleveland = Point(41.499498, -81.695391)
 
-        self.assertAlmostEqual(864.2144943393627, distance(newport, cleveland))
-
-
-if __name__ == "__main__":
-    unittest.main()
+    assert 864.2144943393627 == pytest.approx(distance(newport, cleveland))
