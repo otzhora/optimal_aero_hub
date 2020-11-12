e031a43b5ba564144971862dcbe274b7a80631d9
otzhora <y_rogachev@inbox.ru>
2020-11-12 15:53:46 +0400

    fixes after review

--- hub/metrics.py
+++ hub/metrics.py
@@ -3,11 +3,12 @@ import torch
 from hub.geodesic import Points
 
 
-def exp_cost(hub_coords: torch.Tensor, points: Points) -> torch.Tensor:
+def exp_cost(hub_coords: torch.Tensor, points: Points, C: float = 1.) -> torch.Tensor:
     """
     Calculate cost of placing hub in a hub_coords
+    :param C: impact of distance to result
     :param hub_coords: possible coords of hub
     :param points: list of zones
     :return: cost
     """
-    return (torch.exp(points.distances_to_point(hub_coords) / 1000) * points.normalized_passengers).sum()
+    return (torch.exp(C * points.distances_to_point(hub_coords) / 1000) * points.normalized_passengers).sum()
